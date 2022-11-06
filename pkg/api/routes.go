0312c2d65b61ddff94bbb5274a952b298bde90a0
Alexander Charette <ga_charette@pm.me>
2022-11-06 21:10:02 +0100

    Define basic routes for reward operations

--- pkg/api/routes.go
+++ pkg/api/routes.go
@@ -2,11 +2,14 @@ package api
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 )
+
+// TODO Define expected inputs and outputs
 
 func getReward(name string) (reward *Reward, err error) {
 	filter := bson.D{{"name", name}}
@@ -30,6 +33,52 @@ func getReward(name string) (reward *Reward, err error) {
 	return &result, err
 }
 
-func addReward() {}
+func addReward(rewardJson string) (reward *Reward, err error) {
+	var newReward Reward
+	// TODO Handle errors
+	json.Unmarshal([]byte(rewardJson), &newReward)
+	doc := bson.D{
+		{"name", newReward.Name},
+		{"comment", newReward.Comment},
+		{"icon", newReward.Icon},
+	}
 
-func updateReward() {}
+	result, err := Rewards.InsertOne(context.TODO(), doc)
+	if err != nil {
+		panic(err)
+	}
+
+	return &result, err
+}
+
+func updateReward(rewardJason string) {
+	var updatedReward Reward
+	json.Unmarshal([]byte(rewardJason), &updatedReward)
+	filter := bson.D{
+		{"_id", updatedReward.ID},
+	}
+	// TODO Optimize by only updating changed fields
+	update := bson.D{
+		{
+			"$set", bson.D{
+				{"name", updatedReward.Name},
+				{"comment", updatedReward.Comment},
+				{"icon", updatedReward.Icon},
+			},
+		},
+	}
+	result, err := Rewards.UpdateOne(context.TODO(), filter, update)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func deleteReward(id string) {
+	filter := bson.D{
+		{"_id", id},
+	}
+	result, err := Rewards.DeleteOne(context.TODO(), filter)
+	if err != nil {
+		panic(err)
+	}
+}
